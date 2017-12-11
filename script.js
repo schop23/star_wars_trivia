2c975a037a6ad2cce9180204bcf18e3842967ec7
Shelley Schoppert <schop23@gmail.com>
2017-12-11 16:49:05 -0500

    seventh commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+//answers array
 let answers = ['B', 'A', 'D', 'C', 'D', 'A', 'B', 'C', 'B', 'A', 'C', 'B', 'D', 'A', 'A', 'B', 'B', 'D', 'A', 'C']
+ let score = 0
+ let numQuestions = 20
+ //var for the questions
+ let q1 = document.forms['trivia']['question0'].value
+ 
+ function submitAnswers () {
+     
+ }
