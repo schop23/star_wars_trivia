2429a314abb16d2db632a59c176157a3270ca667
Shelley Schoppert <schop23@gmail.com>
2017-12-12 09:02:39 -0500

    writing questions js

---
+++ script.js
@@ -0,0 +1,28 @@
+// making up the questions
+function () {
+    let questions = [{
+        question: "What was Anakin Skywalker’s mother’s name?",
+        options: ["Alura", "Shmi", "Beru", "Hera"],
+        rightAnswer: "Shmi"
+    }, {
+        question: "Who kills Jabba the Hutt?",
+        options: ["Leia Organa", "C-3PO", "Han Solo", "Luke Skywalker"],
+        rightAnswer: "Leia Organa"
+    }, {
+        question: "What are the odds of successfully navigating an asteroid field according to C-3PO?",
+        options: ["673 to 1", "4828 to 1", "743 to 1", "3720 to 1"],
+        rightAnswer: "3720 to 1"
+    }, {
+        question: "Who is the Jedi that initiated the creation of the Clone Army?",
+        options: ["Count Dooku", "Qui-Gon Jinn", "Sifo-Dyas", "Ki-Adi-Mundi"],
+        rightAnswer: "Sifo-Dyas"
+    }, {
+        question: "How many of the films does Palpatine appear in?",
+        options: ["2", "6", "4", "5"],
+        rightAnswer: "5"
+    }, {
+        question: "The Force Awakens is set how many years after Return of the Jedi?",
+        options: ["30", "25", "17", "40"],
+        rightAnswer: "30"
+    }]
+}
